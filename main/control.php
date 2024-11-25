fb6673bdedf6a18f46bfdefa1aba37d413d6556a
Rajesh Sumbahe <smarthome.india@outlook.com>
2024-11-25 13:10:28 +0100

    Update control.php
    structured and translated

--- main/control.php
+++ main/control.php
@@ -1,4 +1,168 @@
 <?
-/* 
-*/ 
-?><? require(dirname(__FILE__)."/../www/shared/php/config.php"); require(MAIN_PATH."/www/shared/php/base.php"); sql_connect(); $ok=false; if ($argv[1]=='reboot') { if (getEdomiStatus()>0) { setSysInfo(2,13); echo "OK;REBOOT\n"; $ok=true; } } else if ($argv[1]=='shutdown') { if (getEdomiStatus()>0) { setSysInfo(2,23); echo "OK;SHUTDOWN\n"; $ok=true; } } else if ($argv[1]=='activate') { if (getEdomiStatus()>0) { setSysInfo(2,14); echo "OK;ACTIVATE\n"; $ok=true; } } else if ($argv[1]=='start') { if (getEdomiStatus()==1) { setSysInfo(2,11); echo "OK;START\n"; $ok=true; } } else if ($argv[1]=='pause') { if (getEdomiStatus()>=2) { setSysInfo(2,10); createInfoFile(MAIN_PATH.'/www/data/tmp/restartadmin.txt',array('10')); echo "OK;PAUSE\n"; $ok=true; } } else if ($argv[1]=='restart') { if (getEdomiStatus()>=1) { setSysInfo(2,12); createInfoFile(MAIN_PATH.'/www/data/tmp/restartadmin.txt',array('12')); echo "OK;RESTART\n"; $ok=true; } } else if ($argv[1]=='stop' || $argv[1]=='quit') { if (getEdomiStatus()>0) { setSysInfo(2,22); echo "OK;STOP/QUIT;WAITING...\n"; $ok=true; $t=getMicrotime(); while (getSysInfo(1)!=0) { usleep(1000); if (getMicrotime()>=($t+30)) { $t=-1; break; } } if ($t==-1) { echo "ERROR;STOP/QUIT;TIMEOUT\n"; } else { echo "OK;STOP/QUIT;READY\n"; } } } sql_disconnect(); if (!$ok) { echo "ERROR\n"; } ?>
+require(dirname(__FILE__) . "/../../www/shared/php/config.php");
+require(MAIN_PATH . "/www/shared/php/base.php");
+require(MAIN_PATH . "/main/include/php/config.php");
+require(MAIN_PATH . "/main/include/php/base.php");
+require(MAIN_PATH . "/main/include/php/incl_log.php");
+require(MAIN_PATH . "/main/include/php/incl_process.php");
+$main = new procSysinfo();
+$main->exitSelf();
+
+class procSysinfo
+{
+    private $procControl;
+    private $procTrigger;
+    private $procData;
+    private $cpuInfo = false;
+    private $mainTimerOld5 = 0;
+
+    public function __construct()
+    {
+        writeToLog(2, true, 'Process SYSINFO started');
+        sql_connect();
+        $this->procControl = null;
+        $this->procTrigger = procStatus_getTrigger();
+        $this->procData = procStatus_getData(2);
+        $this->procData[19] = 2;
+        $this->proc_run();
+    }
+
+    private function proc_check()
+    {
+        $this->procTrigger = procStatus_getTrigger($this->procTrigger[1]);
+        if ($this->procTrigger[0]) {
+            $this->procControl = procStatus_getControl(2);
+            if ($this->procControl == 1) {
+                writeToLog(2, true, 'stopping process SYSINFO...');
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function proc_run()
+    {
+        $mainTimerOldProc = 0;
+        do {
+            $mainTimerStart = getMicrotime();
+            $this->proc_check();
+            $this->procData[0] = $this->getCpuInfo();
+            if ($this->procData[0] > $this->procData[10] || is_null($this->procData[10])) {
+                $this->procData[10] = $this->procData[0];
+            }
+            if ((intval($mainTimerStart) - intval($this->mainTimerOld5)) >= 5) {
+                $this->mainTimerOld5 = intval($mainTimerStart);
+                if ($n = $this->getRamInfo()) {
+                    $this->procData[1] = $n[2];
+                }
+                if ($this->procData[1] > $this->procData[11] || is_null($this->procData[11])) {
+                    $this->procData[11] = $this->procData[1];
+                }
+                if ($n = getHddSpace('/')) {
+                    $this->procData[2] = 100 - $n[2];
+                }
+                if ($this->procData[2] > $this->procData[12] || is_null($this->procData[12])) {
+                    $this->procData[12] = $this->procData[2];
+                }
+                $this->procData[3] = $this->getSystemloadInfo();
+                if ($this->procData[3] > $this->procData[13] || is_null($this->procData[13])) {
+                    $this->procData[13] = $this->procData[3];
+                }
+                $this->procData[6] = countProcesses('php');
+                if ($this->procData[6] > $this->procData[16] || is_null($this->procData[16])) {
+                    $this->procData[16] = $this->procData[6];
+                }
+                $this->procData[7] = countProcesses('httpd');
+                if ($this->procData[7] > $this->procData[17] || is_null($this->procData[17])) {
+                    $this->procData[17] = $this->procData[7];
+                }
+                $ss1 = sql_call("SHOW OPEN TABLES");
+                if ($n = sql_rowCount($ss1)) {
+                    $this->procData[8] = $n;
+                }
+                if ($this->procData[8] > $this->procData[18] || is_null($this->procData[18])) {
+                    $this->procData[18] = $this->procData[8];
+                }
+                sql_close($ss1);
+            }
+            procStatus_getProcValues($this->procData, $mainTimerOldProc);
+            procStatus_setData(2, $this->procData);
+            if ($this->procControl != 1) {
+                sleep(1);
+            }
+        } while ($this->procControl != 1);
+    }
+
+    private function getCpuInfo()
+    {
+        if (file_exists('/proc/stat') && $lines = file('/proc/stat')) {
+            foreach ($lines as $line) {
+                $n = preg_split('/\s+/', $line);
+                if (strtoupper(array_shift($n)) == 'CPU') {
+                    if ($this->cpuInfo === false) {
+                        $this->cpuInfo = $n;
+                        return 0;
+                    } else {
+                        $tmp[0] = abs($n[0] - $this->cpuInfo[0]);
+                        $tmp[1] = abs($n[1] - $this->cpuInfo[1]);
+                        $tmp[2] = abs($n[2] - $this->cpuInfo[2]);
+                        $tmp[3] = abs($n[3] - $this->cpuInfo[3]);
+                        $this->cpuInfo = $n;
+                        $total = array_sum($tmp);
+                        if ($total > 0) {
+                            return round((($tmp[0] + $tmp[1] + $tmp[2]) / $total * 100), 1);
+                        } else {
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function getRamInfo()
+    {
+        if (file_exists('/proc/meminfo') && $lines = file('/proc/meminfo')) {
+            foreach ($lines as $line) {
+                $n = preg_split('/\s+/', $line);
+                if (strtoupper($n[0]) == 'MEMTOTAL:') {
+                    $mem[0] = intVal($n[1]);
+                }
+                if (strtoupper($n[0]) == 'MEMFREE:') {
+                    $mem[1] = intVal($n[1]);
+                }
+                if (strtoupper($n[0]) == 'BUFFERS:') {
+                    $mem[2] = intVal($n[1]);
+                }
+                if (strtoupper($n[0]) == 'CACHED:') {
+                    $mem[3] = intVal($n[1]);
+                }
+            }
+            $r[0] = $mem[0];
+            $r[1] = $mem[0] - $mem[1] - $mem[2] - $mem[3];
+            if ($r[0] > 0) {
+                $r[2] = truncFloat(($r[1] * 100) / $r[0]);
+            } else {
+                $r[2] = 0;
+            }
+            return $r;
+        } else {
+            return false;
+        }
+    }
+
+    private function getSystemloadInfo()
+    {
+        $n = sys_getloadavg();
+        return round($n[0], 2);
+    }
+
+    public function exitSelf()
+    {
+        writeToLog(2, sql_disconnect(), 'Database: close connection');
+        writeToLog(2, true, 'process SYSINFO stopped');
+        exit();
+    }
+} 
+?>
